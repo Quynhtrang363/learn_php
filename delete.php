08c0b3b77366217e5957271c5123712a751c1545
Quynhtrang363 <132880147+Quynhtrang363@users.noreply.github.com>
2023-07-17 21:14:21 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'pdo.php';
-    $productConnection = new ProductConnection();
-    $id = ['id' => $_POST['id']];
-    $productConnection->deleteProdData($id);
-    header("Location: http://localhost/huyenmy/product/index.php");
-?>
