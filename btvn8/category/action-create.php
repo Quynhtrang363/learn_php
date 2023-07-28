555a002ad369128eed79c3e7979c6ff0a3e6d86d
Quynhtrang363 <132880147+Quynhtrang363@users.noreply.github.com>
2023-07-28 22:46:55 +0700

    Delete btvn8 directory

--- btvn8/category/action-create.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'pdo.php';
-    $categoryConnection = new CategoryConnection();
-    $data = ['name' => $_POST['name']];
-    $categoryConnection->createNewData($data);
-
-    header("Location: http://localhost/huyenmy/category/index.php");
-?>
